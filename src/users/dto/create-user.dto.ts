d5244865f4c45a0e7cfc7e3145b6ebd26045a2f0
Pedro M Boccaletti <boccaletti.pedro@gmail.com>
2023-03-06 10:49:12 -0300

    adds user dtos

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsUUID } from 'class-validator';
+
+export class CreateUserDto {
+  @IsUUID()
+  id: string;
+
+  @IsNotEmpty()
+  userName: string;
+
+  @IsNotEmpty()
+  firstName: string;
+
+  lastName: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+}
