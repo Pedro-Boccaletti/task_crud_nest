b161aa42ffbbab90b4da2bffacc7e76a7c6b99a8
Pedro M Boccaletti <boccaletti.pedro@gmail.com>
2023-03-06 10:57:15 -0300

    changes user route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,7 +11,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller('user')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
