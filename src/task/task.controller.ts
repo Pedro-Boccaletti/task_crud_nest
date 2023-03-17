ac53be539e293b576a7a654cea15cdcaada4a890
Pedro M Boccaletti <boccaletti.pedro@gmail.com>
2023-03-17 10:05:49 -0300

    feat: tasks uses dto now

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,5 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { Task } from '@prisma/client';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 import { TaskService } from './task.service';
 
 @Controller('task')
@@ -7,7 +17,7 @@ export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Post()
-  async postTask(@Body() body: Task) {
+  async postTask(@Body() body: CreateTaskDto) {
     const task = await this.taskService.insertTask(body);
     return task;
   }
@@ -15,5 +25,20 @@ export class TaskController {
   async getAll() {
     const tasks = await this.taskService.getAll();
     return tasks;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.taskService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
+    return this.taskService.update(id, updateTaskDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.taskService.remove(id);
   }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from '@prisma/client';
 import { PrismaService } from '../database/prisma.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TaskService {
@@ -11,10 +12,28 @@ export class TaskService {
     return tasks;
   }
 
-  async insertTask(task: Task) {
+  async insertTask(task: CreateTaskDto) {
     const createdTask = await this.prisma.task.create({
       data: task,
     });
     return createdTask;
+  }
+
+  async findOne(id: string) {
+    const task = await this.prisma.task.findUnique({ where: { id } });
+    return task;
+  }
+
+  async update(id: string, task: UpdateTaskDto) {
+    const updatedTask = await this.prisma.task.update({
+      where: { id },
+      data: { ...task },
+    });
+    return updatedTask;
+  }
+
+  async remove(id: string) {
+    const task = await this.prisma.task.delete({ where: { id } });
+    return task;
   }
 }
