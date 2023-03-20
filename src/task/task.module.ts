0870a2a26b398b7e7acb064bd97938db1387a25c
Pedro M Boccaletti <boccaletti.pedro@gmail.com>
2023-03-20 13:56:42 -0300

    feat: encapsulate prisma in module

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from '../database/prisma.service';
+import { PrismaModule } from '../database/prisma.module';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [TaskController],
-  providers: [TaskService, PrismaService],
+  providers: [TaskService],
 })
 export class TaskModule {}
